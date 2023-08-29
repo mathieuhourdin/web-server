acc54505c0d7a69d4a3a7800a4e439cb214cad77
mathieu <mathieu@fleursdici.fr>
2023-08-29 15:40:33 +0200

    execute migrations at server start

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@ use std::net::{TcpListener};
 use web_server::threadpool::ThreadPool;
 use web_server;
 use tokio::runtime::Runtime;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use web_server::db;
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     let pool = ThreadPool::new(4);
+
+    let mut connection = db::establish_connection();
+    connection.run_pending_migrations(MIGRATIONS).expect("should run migrations if any");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
